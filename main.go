ca632693596199db71a662c5d7e48ec5ae9264fc
Chamz-fdo <fernandohcd.19@uom.lk>
2022-04-09 12:57:51 +0530

    gocommit

--- main.go
+++ main.go
@@ -59,7 +59,20 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Tyoe", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"] 
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func deleteBooks(w http.ResponseWriter, r *http.Request) {
